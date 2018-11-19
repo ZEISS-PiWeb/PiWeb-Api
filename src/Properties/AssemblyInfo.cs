13a7c617e00f70260ad8d696e20a3332c9ef8fb3
Daniel Lahmann <lahmann@zeiss-izm.de>
2018-11-19 15:44:44 +0100

    chore: Adjust assembly version numbers

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 [assembly: AssemblyTrademark( "PiWeb" )]
 [assembly: NeutralResourcesLanguage( "en" )]
 
-[assembly: AssemblyVersion("1.99.3.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-beta1")]
+[assembly: AssemblyVersion( "2.0.0" )]
+[assembly: AssemblyInformationalVersion("2.0.0")]
 
-[assembly: AssemblyFileVersion( "1.99.3.0" )]
+[assembly: AssemblyFileVersion( "2.0.0" )]
