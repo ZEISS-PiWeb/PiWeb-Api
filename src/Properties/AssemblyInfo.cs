2261fd5059ae1ec2a54edfddafe12cadb2c4a82e
Alexander Thiele <thiele@zeiss-izm.de>
2019-07-02 13:31:57 +0200

    Update assembly version and release notes to 5.0.0 (+semver: major)

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 [assembly: AssemblyTrademark( "PiWeb" )]
 [assembly: NeutralResourcesLanguage( "en" )]
 
-[assembly: AssemblyVersion( "4.0.0" )]
-[assembly: AssemblyInformationalVersion("4.0.0")]
+[assembly: AssemblyVersion( "5.0.0" )]
+[assembly: AssemblyInformationalVersion("5.0.0")]
 
-[assembly: AssemblyFileVersion( "4.0.0" )]
+[assembly: AssemblyFileVersion( "5.0.0" )]
