ff887d0282e9501bb4ff3508eb609a05bce7c08e
Michael Dietrich <michael1.dietrich@zeiss.com>
2021-02-16 14:33:20 +0100

    feat: Add supported versions to ServerApiNotSupportedException for better handling

--- src/Api.Rest/Contracts/ServerApiNotSupportedException.cs
+++ src/Api.Rest/Contracts/ServerApiNotSupportedException.cs
@@ -14,9 +14,14 @@ namespace Zeiss.PiWeb.Api.Rest.Contracts
 
 	using System;
 	using System.Runtime.Serialization;
+	using Newtonsoft.Json;
+	using Zeiss.PiWeb.Api.Rest.Dtos;
 
 	#endregion
 
+	/// <summary>
+	/// An exception that is thrown if there is not matching API version found.
+	/// </summary>
 	[Serializable]
 	public class ServerApiNotSupportedException : RestClientException
 	{
@@ -25,8 +30,12 @@ namespace Zeiss.PiWeb.Api.Rest.Contracts
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ServerApiNotSupportedException" /> class.
 		/// </summary>
-		public ServerApiNotSupportedException() : base( "The server supports no known interface version." )
-		{ }
+		/// <param name="versions">The list of offered versions.</param>
+		public ServerApiNotSupportedException( InterfaceVersionRange versions )
+			: base( "The server supports no known interface version." )
+		{
+			Versions = versions ?? throw new ArgumentNullException( nameof( versions ) );
+		}
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ServerApiNotSupportedException" /> class.
@@ -35,9 +44,12 @@ namespace Zeiss.PiWeb.Api.Rest.Contracts
 		/// The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no
 		/// inner exception is specified.
 		/// </param>
-		public ServerApiNotSupportedException( Exception innerException )
+		/// <param name="versions">The list of offered versions.</param>
+		public ServerApiNotSupportedException( InterfaceVersionRange versions, Exception innerException )
 			: base( "The server supports no known interface version.", innerException )
-		{ }
+		{
+			Versions = versions ?? throw new ArgumentNullException( nameof( versions ) );
+		}
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ServerApiNotSupportedException" /> class.
@@ -56,7 +68,27 @@ namespace Zeiss.PiWeb.Api.Rest.Contracts
 		/// </exception>
 		protected ServerApiNotSupportedException( SerializationInfo info, StreamingContext context )
 			: base( info, context )
-		{ }
+		{
+			Versions = JsonConvert.DeserializeObject<InterfaceVersionRange>( info.GetString( nameof( Versions ) ) );
+		}
+
+		#endregion
+
+		/// <summary>
+		/// A list of interface versions that are offered by the server. This value is null if requesting offered
+		/// versions failed.
+		/// </summary>
+		public InterfaceVersionRange Versions { get; }
+
+		#region methods
+
+		/// <inheritdoc />
+		public override void GetObjectData( SerializationInfo info, StreamingContext context )
+		{
+			base.GetObjectData( info, context );
+
+			info.AddValue( nameof( Versions ), JsonConvert.SerializeObject( Versions ) );
+		}
 
 		#endregion
 	}
