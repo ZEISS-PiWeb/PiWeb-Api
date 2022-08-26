990b04e96b50e7b7e1d727a408a60fc7c42b4813
Tamara Geißler <tamara.geissler@zeiss.com>
2022-08-26 17:25:52 +0200

    fix: Return null if the string stored in RawValue is empty

--- src/Api.Rest.Dtos/Data/AttributeDto.cs
+++ src/Api.Rest.Dtos/Data/AttributeDto.cs
@@ -259,26 +259,38 @@ namespace Zeiss.PiWeb.Api.Rest.Dtos.Data
 		{
 			try
 			{
-				DateTime date;
-				if( RawValue is DateTime rawDateTime )
-					date = rawDateTime;
-				else if( RawValue is string value )
-					date = XmlConvert.ToDateTime( value, XmlDateTimeSerializationMode.RoundtripKind );
-				else if( !string.IsNullOrEmpty( _Value ) )
-					date = XmlConvert.ToDateTime( _Value, XmlDateTimeSerializationMode.RoundtripKind );
-				else
+				var date = RawValue switch
+				{
+					DateTime rawDateTime => rawDateTime,
+					string value         => ParseDateValue( value ),
+					_                    => ParseDateValue( _Value )
+				};
+
+				if( date is null )
 					return null;
 
-				if( date.Kind != DateTimeKind.Unspecified )
-					return date.ToUniversalTime();
+				if( date.Value.Kind != DateTimeKind.Unspecified )
+					return date.Value.ToUniversalTime();
 
-				return new DateTime( date.Ticks, DateTimeKind.Local );
+				return new DateTime( date.Value.Ticks, DateTimeKind.Local );
 			}
 			catch
 			{
 				// return null in case of a parse error
 				return null;
 			}
+		}
+
+		/// <summary>
+		/// Parses the string representation of a date to an date time value.
+		/// </summary>
+		/// <param name="value">The string representation of a date.</param>
+		/// <returns>The date time value or <see langword="null"/> if the value could not be parsed.</returns>
+		private static DateTime? ParseDateValue( string value )
+		{
+			if( !string.IsNullOrEmpty( value ) )
+				return XmlConvert.ToDateTime( value, XmlDateTimeSerializationMode.RoundtripKind );
+			return null;
 		}
 
 		private static bool RawValueEquals( [NotNull] object valueX, [NotNull] object valueY )
