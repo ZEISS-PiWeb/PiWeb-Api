d50a778aa8f3761eed5438132afcbb2a0bf73305
Alexander Thiele <51409393+czthiele@users.noreply.github.com>
2024-08-07 14:39:51 +0200

    feat: Add exchangeable OIDC authentication flows
    - add interface abstraction for OIDC flow as well as two implementations, one for the hybrid flow (previously used) and one for the authorization code flow (newly supported)
    - add flags enum to get information about supported flows on server side
    - extract crypto numbers to own record
    - extract validation methods to own class
    - add new OAuthConfigruation class which combines different token information, to allow compatibility since the existing token information cannot be changed easily

---
+++ src/Api.Rest/HttpClient/OAuth/AuthenticationFlows/IOidcAuthenticationFlow.cs
@@ -0,0 +1,50 @@
+﻿#region copyright
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Carl Zeiss Industrielle Messtechnik GmbH        */
+/* Softwaresystem PiWeb                            */
+/* (c) Carl Zeiss 2024                             */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#endregion
+
+namespace Zeiss.PiWeb.Api.Rest.HttpClient.OAuth.AuthenticationFlows;
+
+#region usings
+
+using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Zeiss.PiWeb.Api.Rest.Common.Utilities;
+
+#endregion
+
+/// <summary>
+/// Provides methods for authentication flows using OIDC.
+/// </summary>
+public interface IOidcAuthenticationFlow
+{
+	#region methods
+
+	/// <summary>
+	/// Execute the OIDC authentication flow.
+	/// </summary>
+	/// <param name="refreshToken">Refresh token to acquire a new authentication token.</param>
+	/// <param name="configuration">OAuth configuration containing the settings for authentication.</param>
+	/// <param name="requestCallback">The callback to execute for authentication, e.g opening a browser window.</param>
+	OAuthTokenCredential ExecuteAuthenticationFlow( [CanBeNull] string refreshToken,
+		OAuthConfiguration configuration,
+		Func<OAuthRequest, OAuthResponse> requestCallback );
+
+	/// <summary>
+	/// Asynchronously execute the OIDC authentication flow.
+	/// </summary>
+	/// <param name="refreshToken">Refresh token to acquire a new authentication token.</param>
+	/// <param name="configuration">OAuth configuration containing the settings for authentication.</param>
+	/// <param name="requestCallbackAsync">The asynchronous callback to execute for authentication, e.g opening a browser window.</param>
+	Task<OAuthTokenCredential> ExecuteAuthenticationFlowAsync( [CanBeNull] string refreshToken,
+		OAuthConfiguration configuration,
+		Func<OAuthRequest, Task<OAuthResponse>> requestCallbackAsync );
+
+	#endregion
+}
