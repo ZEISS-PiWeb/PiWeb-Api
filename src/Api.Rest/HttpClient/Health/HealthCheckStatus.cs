a6ab256f87b376930391f827e3284671504f6c4a
Bryan Beck <bryan.beck@zeiss.com>
2025-09-18 08:45:54 +0200

    feat: add HealthServiceRestClient for PiWeb Server health check API

---
+++ src/Api.Rest/HttpClient/Health/HealthCheckStatus.cs
@@ -0,0 +1,36 @@
+﻿#region copyright
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Carl Zeiss Industrielle Messtechnik GmbH        */
+/* Softwaresystem PiWeb                            */
+/* (c) Carl Zeiss 2015                             */
+/* * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#endregion
+
+namespace Zeiss.PiWeb.Api.Rest.HttpClient.Health
+{
+	#region usings
+
+	using System.Text.Json.Serialization;
+	using Newtonsoft.Json;
+
+	#endregion
+
+	/// <summary>
+	/// Holds information about the health check status.
+	/// </summary>
+	public class HealthCheckStatus
+	{
+		#region properties
+
+		/// <summary>
+		/// The overall status of the health check response.
+		/// </summary>
+		[JsonProperty( "status" )]
+		[JsonPropertyName( "status" )]
+		public HealthCheckResultType Status { get; set; }
+
+		#endregion
+	}
+}
